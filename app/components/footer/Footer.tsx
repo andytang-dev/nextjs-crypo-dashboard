2da135ee1500c01e8814d92ac05bfecfc90fbc34
Andy Tang <findme@andytang.dev>
2024-08-16 19:38:12 -0400

    Update group name

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ export const Footer = () => {
     <footer style={{ height: '8rem' }}>
       <hr className="my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25 dark:via-neutral-400" />
       <div className="flex justify-center px-8 items-center">
-        <p style={{ fontSize: '0.8rem', fontWeight: 200 }}>© 2024 aasd4016 group 9. All rights reserved.</p>
+        <p style={{ fontSize: '0.8rem', fontWeight: 200 }}>© 2024 aasd4016 group 3. All rights reserved.</p>
       </div>
     </footer>
   )
